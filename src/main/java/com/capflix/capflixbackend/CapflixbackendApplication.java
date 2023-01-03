6c52e3be72571be3ebf352b2c013ed1035321dc4
jeronimo-pelicoli <jeronimosama@gmail.com>
2023-01-03 10:42:52 -0300

    Initial commit

---
+++ src/main/java/com/capflix/capflixbackend/CapflixbackendApplication.java
@@ -0,0 +1,13 @@
+package com.capflix.capflixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapflixbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapflixbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capflix/capflixbackend/CapflixbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capflix.capflixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapflixbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
